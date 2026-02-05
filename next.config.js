ce3150999972ef254d0728717c5cdb7e3303a3ca
Sergey <133217436+Neolight79@users.noreply.github.com>
2026-02-05 18:42:40 +0300

    chore: remove next.js app files and assets, returned to before-next-js state

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: "export",
-  basePath: "/3d-lifeform-landing",
-  assetPrefix: "/3d-lifeform-landing/"
-};
-
-module.exports = nextConfig;
