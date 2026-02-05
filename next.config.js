bea5e814810e6ae3830ff283247ce534ef2e2eec
Sergey <133217436+Neolight79@users.noreply.github.com>
2026-02-05 18:22:10 +0300

    fix(config): add trailing slash to asset prefix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   basePath: "/3d-lifeform-landing",
-  assetPrefix: "/3d-lifeform-landing"
+  assetPrefix: "/3d-lifeform-landing/"
 };
 
 module.exports = nextConfig;
